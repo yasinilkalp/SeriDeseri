10db75455f60ef0eaaac3330c63cca8d2a27157f
Yasin ilkalp Arabacı <yasinilkalp_24@hotmail.com>
2022-05-09 21:21:20 +0300

    Add project files.

---
+++ Extensions.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+
+namespace SeriDeseri
+{
+    public static class Extensions
+    {
+        public static string SerializeObject<T>(this T request)
+        {
+            return JsonConvert.SerializeObject(request);
+        }
+
+        public static T DeserializeObject<T>(this string request)
+        {
+            return JsonConvert.DeserializeObject<T>(request);
+        }
+    }
+}
